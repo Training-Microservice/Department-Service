e5401860faf2fb72c705cb14cc017f0d9d3a9cef
MSI\Minh Tan <tanlun04112000@gmail.com>
2021-09-30 10:23:20 +0700

    department_service

---
+++ src/main/java/com/qlsv_department_service/QlsvDepartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.qlsv_department_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlsvDepartmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QlsvDepartmentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qlsv_department_service/QlsvDepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qlsv_department_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlsvDepartmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
