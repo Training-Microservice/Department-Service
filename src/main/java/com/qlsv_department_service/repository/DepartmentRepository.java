c1eff8f682fa19fcb2e8cc1f4f19d082f08daa5e
MSI\Minh Tan <tanlun04112000@gmail.com>
2021-09-30 10:30:05 +0700

    add CRUD Khoa

---
+++ src/main/java/com/qlsv_department_service/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.qlsv_department_service.repository;
+
+import com.qlsv_department_service.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    //Department findDepartmentById(Long departmentId);
+}
+
---
+++ src/main/java/com/qlsv_department_service/service/DepartmentService.java
@@ -0,0 +1,26 @@
+package com.qlsv_department_service.service;
+
+import com.qlsv_department_service.entity.Department;
+import com.qlsv_department_service.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+//@RequiredArgsConstructor
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department derpartment) {
+        log.info("Inside saveDepartment of Service! ");
+        return departmentRepository.save(derpartment);
+    }
+
+    public Department findDeparmentById(Long departmentId) {
+        log.info("inside findDeparmentById ");
+        return departmentRepository.findById(departmentId).get();
+    }
+}
