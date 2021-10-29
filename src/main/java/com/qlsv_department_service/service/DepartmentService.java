efb63fb8c802883cb3e3e143bc219d508bf5e5fa
MSI\Minh Tan <tanlun04112000@gmail.com>
2021-10-29 08:24:56 +0700

    CRUD DepartmentService

--- src/main/java/com/qlsv_department_service/service/DepartmentService.java
+++ src/main/java/com/qlsv_department_service/service/DepartmentService.java
@@ -2,6 +2,7 @@ package com.qlsv_department_service.service;
 
 import com.qlsv_department_service.entity.Department;
 import com.qlsv_department_service.repository.DepartmentRepository;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
